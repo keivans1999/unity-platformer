0738ac2d69b4d898061baa95cd3931e28137ea1d
TGF-Coding <calumrscott@outlook.com>
2021-02-05 04:02:30 +0000

    movement

--- platformer/Assets/Scripts/PlayerMovement.cs
+++ platformer/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float movementSpeeed;
+    public Rigidbody2D rb; //velocity and forces 
+    
+    float mx;   //movement for the x-axis
+    //float my;  //movement for the y-axis
+
+    private void Update() 
+    { 
+        mx = Input.GetAxisRaw("Horizontal"); // class to get the value of virtual axis without virtual smoothering. Should get x-axis.
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate() {
+        Vector2 movement = new Vector2(mx * movementSpeeed, rb.velocity.y);
+
+        rb.velocity = movement;
     }
+
+
+
+
+    
+ 
 }
